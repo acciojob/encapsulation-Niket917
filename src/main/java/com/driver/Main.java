16efd711db28fd1b1a99ea5865d251ea80c644af
Niket Sahu <nikkysahu8871@gmail.com>
2023-06-08 23:29:50 +0530

    This is Solution

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,12 @@
 package com.driver;
 
 public class Main {
-  
+
+    public static void main(String[] args) {
+        RWOnly obj1 = new RWOnly();
+        obj1.setName("Niket");
+        obj1.setAge(23);
+
+        System.out.println(obj1.getName()+"\n"+obj1.getAge()+"\n");
+    }
 }
